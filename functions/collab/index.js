04249fe8c362d3e7000982fbf0f81dbe37eec474
Madhavan <59589123+madhavanrayzz@users.noreply.github.com>
2026-01-28 15:17:21 +0530

    Update index.js

--- functions/collab/index.js
+++ functions/collab/index.js
@@ -1,10 +1,5 @@
 export async function onRequest({ request }) {
-  console.log({
-    method: request.method,
-    url: request.url,
-    headers: Object.fromEntries(request.headers),
-    time: new Date().toISOString()
-  });
+  console.log("HIT", request.url, new Date().toISOString());
 
   return new Response("OK", { status: 200 });
 }
